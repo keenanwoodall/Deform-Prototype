f8b01bdaf7a2b9563418bfd16e2bd6a7d1564578
keenanwoodall <keenanwoodall@gmail.com>
2018-02-21 12:12:25 -0600

    Small refactor
    Cut down on some of the CreateChunks method code.

--- Assets/DForm/Code/Utility/ChunkUtil.cs
+++ Assets/DForm/Code/Utility/ChunkUtil.cs
@@ -27,51 +27,7 @@ namespace DForm
 
 		public static Chunk[] CreateChunks (Mesh mesh, int count)
 		{
-			// Cache the mesh data.
-			var vertices = mesh.vertices;
-			var normals = mesh.normals;
-
-			// Create the array of chunks.
-			var chunks = new Chunk[count];
-			// Cache chunk info.
-			var vertexCount = mesh.vertexCount;
-			var chunkSize = vertexCount / count;
-
-			if (count > vertexCount)
-			{
-				Debug.LogWarning ("Chunk count is greater than vertex count.");
-				count = vertexCount;
-			}
-
-			// Loop through each chunk.
-			for (var chunkIndex = 0; chunkIndex < count; chunkIndex++)
-			{
-				// Calculate the start and end index of the chunk
-				var startIndex = chunkSize * chunkIndex;
-				var endIndex = chunkSize * (chunkIndex + 1);
-				if (chunkIndex + 1 == count)
-					endIndex += vertexCount - (chunkSize * count);
-
-				// Create the arrays to hold the chunk data.
-				var chunkLength = endIndex - startIndex;
-				var chunkPositions = new Vector3[chunkLength];
-				var chunkNormals = new Vector3[chunkLength];
-
-				// Loop through each vertex in the chunk.
-				var chunkVertexIndex = 0;
-				for (var vertexIndex = startIndex; vertexIndex < endIndex; vertexIndex++)
-				{
-					// Put the current vertex data into the chunk arrays.
-					chunkPositions[chunkVertexIndex] = vertices[vertexIndex];
-					chunkNormals[chunkVertexIndex] = normals[vertexIndex];
-					chunkVertexIndex++;
-				}
-
-				// Create a chunk from the chunk arrays.
-				chunks[chunkIndex] = new Chunk (chunkPositions, chunkNormals);
-			}
-
-			return chunks;
+			return CreateChunks (VertexDataUtil.GetVertexData (mesh), count);
 		}
 
 		public static Chunk[] CreateChunks (VertexData[] vertexData, int count)
