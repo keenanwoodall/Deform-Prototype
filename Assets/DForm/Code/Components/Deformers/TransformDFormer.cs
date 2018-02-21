688fe87382e07147da3c451addc2bba4f5aa97d3
keenanwoodall <keenanwoodall@gmail.com>
2018-02-21 17:08:43 -0600

    Created the Transform deformer
    And deleted the position one

---
+++ Assets/DForm/Code/Components/Deformers/TransformDFormer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace DForm.DFormers
+{
+	public class TransformDFormer : DFormerComponent
+	{
+		public Vector3 position;
+		public Vector3 rotation;
+		public Vector3 scale;
+
+		private Matrix4x4 transformSpace;
+
+		public override void PreModify ()
+		{
+			transformSpace = Matrix4x4.identity;
+			transformSpace *= Matrix4x4.TRS (position, Quaternion.Euler (rotation), scale);
+		}
+
+		public override VertexData[] Modify (VertexData[] vertexData)
+		{
+			for (var vertexIndex = 0; vertexIndex < vertexData.Length; vertexIndex++)
+				vertexData[vertexIndex].position = transformSpace.MultiplyPoint3x4 (vertexData[vertexIndex].position);
+
+			return vertexData;
+		}
+	}
+}
