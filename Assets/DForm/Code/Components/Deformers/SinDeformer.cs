8862c84f2c84dc55c5b413cc99b2d2a07dcc5619
keenanwoodall <keenanwoodall@gmail.com>
2018-02-22 02:30:38 -0600

    Added Sin deformer

--- Assets/DForm/Code/Components/Deformers/SinDeformer.cs
+++ Assets/DForm/Code/Components/Deformers/SinDeformer.cs
@@ -1,9 +1,74 @@
 ﻿using UnityEngine;
+using Deform.Math.Trig;
 
 namespace Deform.Deformers
 {
-	public class SinDeformer : MonoBehaviour
+	public class SinDeformer : DeformerComponent
 	{
+		public float offset;
+		public float speed;
+		public bool useWorldPosition;
 
+		public Axis along;
+		public Axis by;
+
+		public Sin sin = new Sin ();
+
+		private Vector3 axisOffset;
+
+		public override void PreModify ()
+		{
+			switch (along)
+			{
+				case Axis.X:
+					axisOffset = Vector3.right * offset;
+					break;
+				case Axis.Y:
+					axisOffset = Vector3.up * offset;
+					break;
+				case Axis.Z:
+					axisOffset = Vector3.forward * offset;
+					break;
+			}
+		}
+
+		public override VertexData[] Modify (VertexData[] vertexData)
+		{
+			if (useWorldPosition)
+				for (var vertexIndex = 0; vertexIndex < vertexData.Length; vertexIndex++)
+					vertexData[vertexIndex].position += Sin3D (vertexData[vertexIndex].position + transform.position + axisOffset);
+			else
+				for (var vertexIndex = 0; vertexIndex < vertexData.Length; vertexIndex++)
+					vertexData[vertexIndex].position += Sin3D (vertexData[vertexIndex].position + axisOffset);
+
+			return vertexData;
+		}
+
+		private Vector3 Sin3D (Vector3 sample)
+		{
+			var animatedOffset = offset + speed * Time.time;
+			var byValue = 0f;
+			switch (by)
+			{
+				case Axis.X:
+					byValue = sample.x;
+					break;
+				case Axis.Y:
+					byValue = sample.y;
+					break;
+				case Axis.Z:
+					byValue = sample.z;
+					break;
+			}
+			switch (along)
+			{
+				case Axis.X:
+					return new Vector3 (sin.Solve (byValue, animatedOffset), 0f, 0f);
+				case Axis.Y:
+					return new Vector3 (0f, sin.Solve (byValue, animatedOffset), 0f);
+				default:
+					return new Vector3 (0f, 0f, sin.Solve (byValue, animatedOffset));
+			}
+		}
 	}
 }
