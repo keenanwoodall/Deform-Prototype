b2f87b82b643651bc54fc64bf029b58886b84c55
keenanwoodall <keenanwoodall@gmail.com>
2018-03-02 18:32:02 -0600

    Added a center pivot deformer
    This deformer centers the mesh on its pivot.

---
+++ Assets/Deform/Code/Components/Deformers/CenterPivotDeformer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Deform.Deformers
+{
+	public class CenterPivotDeformer : DeformerComponent
+	{
+		public override Chunk Modify (Chunk chunk)
+		{
+			for (var vertexIndex = 0; vertexIndex < chunk.Size; vertexIndex++)
+			{
+				chunk.vertexData[vertexIndex].position -= chunk.bounds.center;
+			}
+
+			return chunk;
+		}
+	}
+}
