2c45aca6d860aa784276a0aa1d708388dd388c51
keenanwoodall <keenanwoodall@gmail.com>
2018-03-07 01:33:45 -0600

    Added scale along axis deformer

---
+++ Assets/Deform/Code/Components/Deformers/ScaleAlongAxisDeformer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Deform.Deformers
+{
+	public class ScaleAlongAxisDeformer : DeformerComponent
+	{
+		public Vector3 scale = Vector3.one;
+		public Transform axis;
+
+		private Matrix4x4 axisSpace;
+		private Matrix4x4 inverseAxisSpace;
+
+		public override void PreModify ()
+		{
+			base.PreModify ();
+
+			if (axis == null)
+			{
+				axis = new GameObject ("ScaleAxis").transform;
+				axis.SetParent (transform);
+				axis.localPosition = Vector3.zero;
+				axis.Rotate (-90f, 0f, 0f);
+			}
+
+			axisSpace = Matrix4x4.TRS (Vector3.zero, Quaternion.Inverse (axis.rotation) * transform.rotation, Vector3.one);
+			inverseAxisSpace = axisSpace.inverse;
+		}
+
+		public override Chunk Modify (Chunk chunk, TransformData transformData, Bounds bounds)
+		{
+			for (int vertexIndex = 0; vertexIndex < chunk.Size; vertexIndex++)
+			{
+				var position = axisSpace.MultiplyPoint3x4 (chunk.vertexData[vertexIndex].position);
+				position.Scale (scale);
+				chunk.vertexData[vertexIndex].position = inverseAxisSpace.MultiplyPoint3x4 (position);
+			}
+
+			return chunk;
+		}
+	}
+}
