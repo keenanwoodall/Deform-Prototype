b2ae486302da9bae20eb0f8d3fb3007909bb898c
keenanwoodall <keenanwoodall@gmail.com>
2018-03-09 16:45:59 -0600

    Spherify improvements

--- Assets/Deform/Code/Components/Deformers/SpherifyDeformer.cs
+++ Assets/Deform/Code/Components/Deformers/SpherifyDeformer.cs
@@ -5,17 +5,18 @@ namespace Deform.Deformers
 	public class SpherifyDeformer : DeformerComponent
 	{
 		public float radius = 1f;
+		[Range (0f, 1f)]
 		public float strength = 0f;
 		public Vector3 offset;
 
 		public override VertexData[] Modify (VertexData[] vertexData, TransformData transformData, Bounds meshBounds)
 		{
-			var boundsSize = meshBounds.size.sqrMagnitude;
+			var boundsSize = meshBounds.size.magnitude * 0.5f;
 			for (int vertexIndex = 0; vertexIndex < vertexData.Length; vertexIndex++)
 			{
-				vertexData[vertexIndex].position = Vector3.LerpUnclamped (
+				vertexData[vertexIndex].position = Vector3.Lerp (
 					vertexData[vertexIndex].position,
-					meshBounds.center + offset + ((vertexData[vertexIndex].position - meshBounds.center).normalized * radius * boundsSize),
+					meshBounds.center + offset + ((vertexData[vertexIndex].position - meshBounds.center).normalized * (radius * boundsSize)),
 					strength);
 			}
 
