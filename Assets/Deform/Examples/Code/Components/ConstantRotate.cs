a81ace9f102f242a8ffdeedfbd031b4c6e28c929
Keenan Woodall <keenanwoodall@gmail.com>
2018-04-07 14:25:33 -0500

    Added example scenes for most of the deformers.

---
+++ Assets/Deform/Examples/Code/Components/ConstantRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ConstantRotate : MonoBehaviour
+{
+	public Vector3 amount;
+	public Space space;
+
+	private void Update ()
+	{
+		transform.Rotate (amount * Time.deltaTime, space);
+	}
+}
