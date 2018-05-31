84d9172d81af4363c85226f6eb4e3119c0a2f862
Keenan Woodall <keenanwoodall@gmail.com>
2018-05-31 16:43:36 -0500

    Made some long comment lines into two lines.

--- Assets/Deform/Code/Components/Bases/DeformerComponent.cs
+++ Assets/Deform/Code/Components/Bases/DeformerComponent.cs
@@ -43,12 +43,14 @@ namespace Deform
 		public virtual void PreModify () { }
 		/// <summary>
 		/// Deform the vertex data in this method by modifying the vertexData argument and returning it.
-		/// This method is might be running on another thread (if Update Mode is set to UpdateAsync) so you can't access things like transforms or anything else Unity manages.
+		/// This method is might be running on another thread (if Update Mode is set to UpdateAsync) so you can't access 
+		/// things like transforms or anything else Unity manages.
 		/// If you need access to something and Unity won't let you get it, override and cache it in the PreModify method.
 		/// </summary>
 		/// <param name="transformData">The current transform data of the mesh.</param>
 		/// <param name="vertexDataBounds">The bounds of the current vertex data.</param>
-		/// <returns>The vertexData you return will be sent to the next deformer component or applied to the mesh if this is the last deformer component.</returns>
+		/// <returns>The vertexData you return will be sent to the next deformer component or applied to the mesh if this 
+		/// is the last deformer component.</returns>
 		public abstract VertexData[] Modify (VertexData[] vertexData, TransformData transformData, Bounds vertexDataBounds);
 		/// <summary>
 		/// Called right after Modify done being called on all deformer components.
