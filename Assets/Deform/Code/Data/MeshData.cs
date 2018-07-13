ac680549e65eda8c6f4c379318e6b3b48d35ed19
Keenan Woodall <keenanwoodall@gmail.com>
2018-07-12 21:51:25 -0500

    Created NativeMeshData struct and MeshData class.

---
+++ Assets/Deform/Code/Data/MeshData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Deform.Data
+{
+	public class MeshData
+	{
+		public Vector3[] vertices;
+		public Vector3[] normals;
+		public Vector4[] tangents;
+		public Vector2[] uv;
+
+		public MeshData (Mesh mesh)
+		{
+			vertices = mesh.vertices;
+			normals = mesh.normals;
+			tangents = mesh.tangents;
+			uv = mesh.uv;
+		}
+	}
+}
---
+++ Assets/Deform/Code/Data/NativeMeshData.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Collections;
+using UnityEngine;
+
+namespace Deform.Data
+{
+	public struct NativeMeshData
+	{
+		public NativeArray<Vector3> vertices;
+		public NativeArray<Vector3> normals;
+		public NativeArray<Vector4> tangents;
+		public NativeArray<Vector2> uv;
+
+
+		public NativeMeshData (MeshData data, Allocator allocator = Allocator.Temp)
+		{
+			vertices = new NativeArray<Vector3> (data.vertices, allocator);
+			normals = new NativeArray<Vector3> (data.normals, allocator);
+			tangents = new NativeArray<Vector4> (data.tangents, allocator);
+			uv = new NativeArray<Vector2> (data.uv, allocator);
+		}
+
+		public void CopyTo (MeshData data)
+		{
+			vertices.CopyTo (data.vertices);
+			normals.CopyTo (data.normals);
+			tangents.CopyTo (data.tangents);
+			uv.CopyTo (data.uv);
+		}
+
+		public void Dispose ()
+		{
+			vertices.Dispose ();
+			normals.Dispose ();
+			tangents.Dispose ();
+			uv.Dispose ();
+		}
+	}
+}
