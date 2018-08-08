39c6f271380e4beccd0711eec4b598474b92f4d6
Keenan Woodall <keenanwoodall@gmail.com>
2018-08-08 12:25:43 -0500

    Shortened the time it takes to/ convert to/from native data by using unsafe code (memcpy). Hell yea!

--- Assets/Deform/Code/Data/NativeMeshData.cs
+++ Assets/Deform/Code/Data/NativeMeshData.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 namespace Deform.Data
@@ -14,31 +15,37 @@ namespace Deform.Data
 
 		public NativeMeshData (MeshData data, Allocator allocator = Allocator.Temp)
 		{
-			vertices = new NativeArray<Vector3> (data.vertices, allocator);
-			normals = new NativeArray<Vector3> (data.normals, allocator);
-			tangents = new NativeArray<Vector4> (data.tangents, allocator);
-			uv = new NativeArray<Vector2> (data.uv, allocator);
+			var length = data.size;
+			vertices = new NativeArray<Vector3> (length, allocator, NativeArrayOptions.UninitializedMemory);
+			normals = new NativeArray<Vector3> (length, allocator, NativeArrayOptions.UninitializedMemory);
+			tangents = new NativeArray<Vector4> (length, allocator, NativeArrayOptions.UninitializedMemory);
+			uv = new NativeArray<Vector2> (length, allocator, NativeArrayOptions.UninitializedMemory);
 
 			size = data.vertices.Length;
+
+			GetNativeVector3Arrays (data.vertices, vertices);
+			GetNativeVector3Arrays (data.normals, normals);
+			GetNativeVector4Arrays (data.tangents, tangents);
+			GetNativeVector2Arrays (data.uv, uv);
 		}
 
 		public void CopyTo (MeshData data)
 		{
-			vertices.CopyTo (data.vertices);
-			normals.CopyTo (data.normals);
-			tangents.CopyTo (data.tangents);
-			uv.CopyTo (data.uv);
+			SetNativeVector3Array (data.vertices, vertices);
+			SetNativeVector3Array (data.normals, normals);
+			SetNativeVector4Array (data.tangents, tangents);
+			SetNativeVector2Array (data.uv, uv);
 		}
 		public void CopyTo (MeshData data, bool copyVertices = true, bool copyNormals = false, bool copyTangents = false, bool copyUv = false)
 		{
 			if (copyVertices)
-				vertices.CopyTo (data.vertices);
+				SetNativeVector3Array (data.vertices, vertices);
 			if (copyNormals)
-				normals.CopyTo (data.normals);
+				SetNativeVector3Array (data.normals, normals);
 			if (copyTangents)
-				tangents.CopyTo (data.tangents);
+				SetNativeVector4Array (data.tangents, tangents);
 			if (copyTangents)
-				uv.CopyTo (data.uv);
+				SetNativeVector2Array (data.uv, uv);
 		}
 
 		public void Dispose ()
@@ -51,6 +58,53 @@ namespace Deform.Data
 				tangents.Dispose ();
 			if (uv.IsCreated)
 				uv.Dispose ();
+		}
+
+		private unsafe void GetNativeVector2Arrays (Vector2[] input, NativeArray<Vector2> output)
+		{
+			fixed (void* vertexBufferPointer = input)
+			{
+				UnsafeUtility.MemCpy (NativeArrayUnsafeUtility.GetUnsafeBufferPointerWithoutChecks (output),
+					vertexBufferPointer, input.Length * (long)UnsafeUtility.SizeOf<Vector2> ());
+			}
+
+		}
+		unsafe void SetNativeVector2Array (Vector2[] vertexArray, NativeArray<Vector2> vertexBuffer)
+		{
+			fixed (void* vertexArrayPointer = vertexArray)
+			{
+				UnsafeUtility.MemCpy (vertexArrayPointer, NativeArrayUnsafeUtility.GetUnsafeBufferPointerWithoutChecks (vertexBuffer), vertexArray.Length * (long)UnsafeUtility.SizeOf<Vector2> ());
+			}
+		}
+		private unsafe void GetNativeVector3Arrays (Vector3[] input, NativeArray<Vector3> output)
+		{
+			fixed (void* vertexBufferPointer = input)
+			{
+				UnsafeUtility.MemCpy (NativeArrayUnsafeUtility.GetUnsafeBufferPointerWithoutChecks (output),
+					vertexBufferPointer, input.Length * (long)UnsafeUtility.SizeOf<Vector3> ());
+			}
+		}
+		unsafe void SetNativeVector3Array (Vector3[] vertexArray, NativeArray<Vector3> vertexBuffer)
+		{
+			fixed (void* vertexArrayPointer = vertexArray)
+			{
+				UnsafeUtility.MemCpy (vertexArrayPointer, NativeArrayUnsafeUtility.GetUnsafeBufferPointerWithoutChecks (vertexBuffer), vertexArray.Length * (long)UnsafeUtility.SizeOf<Vector3> ());
+			}
+		}
+		private unsafe void GetNativeVector4Arrays (Vector4[] input, NativeArray<Vector4> output)
+		{
+			fixed (void* vertexBufferPointer = input)
+			{
+				UnsafeUtility.MemCpy (NativeArrayUnsafeUtility.GetUnsafeBufferPointerWithoutChecks (output),
+					vertexBufferPointer, input.Length * (long)UnsafeUtility.SizeOf<Vector4> ());
+			}
+		}
+		unsafe void SetNativeVector4Array (Vector4[] vertexArray, NativeArray<Vector4> vertexBuffer)
+		{
+			fixed (void* vertexArrayPointer = vertexArray)
+			{
+				UnsafeUtility.MemCpy (vertexArrayPointer, NativeArrayUnsafeUtility.GetUnsafeBufferPointerWithoutChecks (vertexBuffer), vertexArray.Length * (long)UnsafeUtility.SizeOf<Vector4> ());
+			}
 		}
 	}
 }
