556f6a66910685282283f3e8b85b9b7b15addc1c
Keenan Woodall <keenanwoodall@gmail.com>
2018-09-15 14:28:15 -0500

    Added different noise spaces: derivative (original), local, normal, color

--- Assets/Deform/Code/Component/Deformers/NoiseDeformer.cs
+++ Assets/Deform/Code/Component/Deformers/NoiseDeformer.cs
@@ -11,10 +11,17 @@ namespace Deform.Deformers
 {
 	public class NoiseDeformer : Deformer
 	{
+		// General noise settings
 		public float magnitude = 0.1f;
 		public float frequency = 1f;
 		public float speed = .1f;
-		public float offset = 0f;
+		public Vector3 offset = Vector3.zero;
+		public enum NoiseSpace { Derivative, Local, Normal, Color }
+		public NoiseSpace noiseSpace;
+		[Header ("Local Noise Space Settings")]
+		public Vector3 localNoiseDirection = Vector3.up;
+		[Header ("Color Noise Space Settings")]
+		public bool alphaInfluence = true;
 
 		private float speedOffset;
 
@@ -25,21 +32,53 @@ namespace Deform.Deformers
 
 		public override JobHandle Deform (NativeMeshData data, JobHandle dependency)
 		{
-			return new DeformJob ()
+			switch (noiseSpace)
 			{
-				magnitude = magnitude,
-				frequency = frequency,
-				offset = offset + speedOffset,
-				data = data
-			}.Schedule (data.size, BATCH_COUNT, dependency);
+				case NoiseSpace.Derivative:
+					return new DerivativeDeformJob ()
+					{
+						magnitude = magnitude,
+						frequency = frequency,
+						offset = offset + (Vector3.one * speedOffset),
+						data = data
+					}.Schedule (data.size, BATCH_COUNT, dependency);
+				case NoiseSpace.Local:
+					return new LocalDeformJob ()
+					{
+						magnitude = magnitude,
+						frequency = frequency,
+						offset = offset + (Vector3.one * speedOffset),
+						direction = localNoiseDirection,
+						data = data
+					}.Schedule (data.size, BATCH_COUNT, dependency);
+				case NoiseSpace.Normal:
+					return new NormalDeformJob ()
+					{
+						magnitude = magnitude,
+						frequency = frequency,
+						offset = offset + (Vector3.one * speedOffset),
+						data = data
+					}.Schedule (data.size, BATCH_COUNT, dependency);
+				case NoiseSpace.Color:
+					return new ColorDeformJob ()
+					{
+						magnitude = magnitude,
+						frequency = frequency,
+						offset = offset + (Vector3.one * speedOffset),
+						alphaInfluence = alphaInfluence,
+						data = data
+					}.Schedule (data.size, BATCH_COUNT, dependency);
+				default:
+					throw new System.Exception (string.Format ("{0} is not a suppported noise space.", noiseSpace.ToString ()));
+			}
 		}
 
 		[BurstCompile]
-		private struct DeformJob : IJobParallelFor
+		private struct DerivativeDeformJob : IJobParallelFor
 		{
 			public float magnitude;
 			public float frequency;
-			public float offset;
+			public float3 offset;
 			public NativeMeshData data;
 
 			public void Execute (int index)
@@ -48,10 +87,65 @@ namespace Deform.Deformers
 
 				var noiseOffset = float3 
 				(
-					noise.cnoise (float3 (position.y, position.z, offset) * frequency),
-					noise.cnoise (float3 (position.x, position.z, offset) * frequency),
-					noise.cnoise (float3 (position.x, position.y, offset) * frequency)
+					noise.cnoise (float3 (position.y + offset.y, position.z + offset.z, offset.x) * frequency),
+					noise.cnoise (float3 (position.x + offset.x, position.z + offset.z, offset.y) * frequency),
+					noise.cnoise (float3 (position.x + offset.x, position.y + offset.y, offset.z) * frequency)
 				) * magnitude;
+
+				data.vertices[index] = position + noiseOffset;
+			}
+		}
+
+		[BurstCompile]
+		private struct LocalDeformJob : IJobParallelFor
+		{
+			public float magnitude;
+			public float frequency;
+			public float3 offset;
+			public float3 direction;
+			public NativeMeshData data;
+
+			public void Execute (int index)
+			{
+				var position = data.vertices[index];
+
+				var noiseOffset = (direction * noise.cnoise (float3 (position.x + offset.x, position.y + offset.y, position.z + offset.z) * frequency)) * magnitude;
+
+				data.vertices[index] = position + noiseOffset;
+			}
+		}
+
+		[BurstCompile]
+		private struct NormalDeformJob : IJobParallelFor
+		{
+			public float magnitude;
+			public float frequency;
+			public float3 offset;
+			public NativeMeshData data;
+
+			public void Execute (int index)
+			{
+				var position = data.vertices[index];
+
+				var noiseOffset = (data.normals[index] * noise.cnoise (float3 (position.x + offset.x, position.y + offset.y, position.z + offset.z) * frequency)) * magnitude;
+
+				data.vertices[index] = position + noiseOffset;
+			}
+		}
+		[BurstCompile]
+		private struct ColorDeformJob : IJobParallelFor
+		{
+			public float magnitude;
+			public float frequency;
+			public float3 offset;
+			public bool alphaInfluence;
+			public NativeMeshData data;
+
+			public void Execute (int index)
+			{
+				var position = data.vertices[index];
+				var color = data.colors[index];
+				var noiseOffset = color.xyz * noise.cnoise (float3 (position.x + offset.x, position.y + offset.y, position.z + offset.z) * frequency) * magnitude * (alphaInfluence ? color.w : 1f);
 
 				data.vertices[index] = position + noiseOffset;
 			}
